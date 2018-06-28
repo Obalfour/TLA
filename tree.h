58d085263f58488a253d0ae32435ec8ff9a991c9
Oliver <obalfour@itba.edu.ar>
2018-06-28 15:53:41 -0300

    new name for nodestruct

--- tree.h
+++ tree.h
@@ -1,8 +1,8 @@
 #define MAXNODES 4
 
-typedef struct nodeStruct{
+typedef struct Node{
 	char* nodeValue;         
-	struct nodeStruct * * children;
+	struct Node * * children;
 	int child;   
  	char* nodeType;
 }Node;
